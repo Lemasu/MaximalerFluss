617512bf15eeab8eead5372db46eb72b1d1f91d8
Lemasu <alex.schaub@lernende.bbw.ch>
2022-02-09 21:43:53 +0100

    Der Inhalt der beiden Klassen Point und Line sollen kommentiert sein.

--- src/main/java/ch/kbw/maximalerfluss/Line.java
+++ src/main/java/ch/kbw/maximalerfluss/Line.java
@@ -4,28 +4,67 @@ package ch.kbw.maximalerfluss;
  * Dies ist die Klasse fuer die Kanten in einem Graphen.
  * 
  * Diese Klasse basiert auf das Projekt der Gruppe "Zyklensuche".
+ * 
+ * @author Alex Schaub
  */
 public class Line {
-    private Point p1, p2;
+	/**
+	 * Das ist der erste Knoten dieser Kante.
+	 */
+    private Point p1;
+    
+    /**
+     * Das ist der zweite Knoten dieser Kante.
+     */
+    private Point p2;
+    
+    /**
+     * Das ist das Gewicht dieser Kante.
+     */
     private int weight;
 
+    /**
+     * Das ist ein Konstruktor.
+     * 
+     * @param p1 Das ist der erste Knote dieser Kante.
+     * @param p2 Das ist der zweite Knote dieser Kante.
+     * @param weight Das ist das Gewicht dieser Kante.
+     */
     public Line(Point p1, Point p2, int weight) {
         this.p1 = p1;
         this.p2 = p2;
         this.weight = weight;
     }
 
+    /**
+     * Das ist der Standardkonstruktor.
+     */
     public Line() {
     }
 
+    /**
+     * Das ist der Getter fuer den ersten Knoten dieser Kante.
+     * 
+     * @return Das ist der erste Knote dieser Kante.
+     */
     public Point getP1() {
         return p1;
     }
 
+    /**
+     * Das ist der Getter fuer den zweiten Knoten dieser Kante.
+     * 
+     * @return Das ist der zweite Knote dieser Kante.
+     */
     public Point getP2() {
         return p2;
     }
 
+    /**
+     * Das ist der Getter fuer den Gewicht dieser Kante.
+     * 
+     * @return Das ist das Gewicht dieser Kante.
+     */
     public int getWeight() {
         return weight;
     }
