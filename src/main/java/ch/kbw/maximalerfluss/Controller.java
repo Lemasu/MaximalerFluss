5a5b6e8b7fc595fe37c90c10f53e61782e95bb98
Lemasu <alex.schaub@lernende.bbw.ch>
2022-02-16 16:11:43 +0100

    Der Start und das Ziel sollen markiert werden.

--- src/main/java/ch/kbw/maximalerfluss/Controller.java
+++ src/main/java/ch/kbw/maximalerfluss/Controller.java
@@ -97,7 +97,7 @@ public class Controller {
 	 */
 	private void graphZeichnen() {
 		// Erstelle die benoetigten ArrayLists.
-		
+
 		// Das sind die Knoten des Graphen.
 		ArrayList<ArrayList<Knoten>> knoten = model.getGraph().getKnoten();
 		// Das sind die Positionen in X-Richtung der Knoten.
@@ -106,19 +106,22 @@ public class Controller {
 		ArrayList<ArrayList<Integer>> y_positionen = new ArrayList<ArrayList<Integer>>();
 		// Das sind die Kanten des Graphen.
 		ArrayList<Kante> kanten = model.getGraph().getKanten();
-
-		// setzte die Farben fuer Fill und Stroke auf Schwarz
+		
+		// Das ist die Groesse eines Kreises.
+		int kreisgroesse = 20;
+		
+		// setzt die Farbe fuer Fill auf Schwarz
 		gc.setFill(Color.BLACK);
-		gc.setStroke(Color.BLACK);
 
 		// Diese Schleife zeichnet die Knoten.
 		for (int i = 0; i < knoten.size(); i++) {
 			// initialisiere die benoetigten Variablen
-			
+
 			/*
 			 * Das ist die X-Position eines Knotens.
 			 * 
-			 * Das Minimum fuer die X-Position ist fuenf. Danach wird immer jeweils 50 zu der letzten X-Position hinzuaddiert.
+			 * Das Minimum fuer die X-Position ist fuenf. Danach wird immer jeweils 50 zu
+			 * der letzten X-Position hinzuaddiert.
 			 */
 			int x_position = 5 + (50 * i);
 			/*
@@ -132,19 +135,23 @@ public class Controller {
 			 * 
 			 * Die Groesse eines Schrittes haengt von der Laenge einer waagerechte Ebene ab.
 			 */
-			int y_position_zaehlschritte = 400 / (knoten.get(i).size() + 1);
-			
+			int y_position_zaehlschritte = y_position / (knoten.get(i).size() + 1);
+
 			// erstelle neue ArrayLists fuer die aktuelle waagerechte Ebene
 			x_positionen.add(new ArrayList<Integer>());
 			y_positionen.add(new ArrayList<Integer>());
 
 			// Schleife, um die einzelnen Knoten einer waagerechte Ebene zu zeichnen
 			for (int j = 0; j < knoten.get(i).size(); j++) {
-				// Ziehe einen Zaehlschritt von der Y-Position ab, damit die Y-Position die richtige Groesse hat.
+				/*
+				 * Ziehe einen Zaehlschritt von der Y-Position ab, damit die Y-Position die
+				 * richtige Groesse hat.
+				 */
 				y_position -= y_position_zaehlschritte;
-				// zeichne den Knoten
-				gc.fillOval(x_position, y_position, 20, 20);
 				
+				// zeichne den Knoten
+				gc.fillOval(x_position, y_position, kreisgroesse, kreisgroesse);
+
 				// speichere die Position des Knotens ab
 				x_positionen.get(x_positionen.size() - 1).add(x_position);
 				y_positionen.get(y_positionen.size() - 1).add(y_position);
@@ -154,21 +161,25 @@ public class Controller {
 		// Schleife, um die einzelnen Kanten zu zeichnen
 		for (Kante kante : kanten) {
 			/*
-			 * Speichere die ID der aktuellen Knoten ab, um spaeter wieder darauf zuzugreifen zu koennen.
+			 * Speichere die ID der aktuellen Knoten ab, um spaeter wieder darauf
+			 * zuzugreifen zu koennen.
 			 * 
-			 * Die IDs wurden als Strings abgespeichert. Die waagerechten Ebenen und die senkrechten Ebenen wurden mit einem Punkt getrennt.
+			 * Die IDs wurden als Strings abgespeichert. Die waagerechten Ebenen und die
+			 * senkrechten Ebenen wurden mit einem Punkt getrennt.
 			 * 
-			 * Um die ID fuer die spaetere Verwendung optimal abzuspeichern, wurden die beiden Teile der ID aufgeteilt und der Punkt entfernt.
+			 * Um die ID fuer die spaetere Verwendung optimal abzuspeichern, wurden die
+			 * beiden Teile der ID aufgeteilt und der Punkt entfernt.
 			 */
 			String[] knoten_1 = kante.getKnoten_1().getId().split("\\.");
 			String[] knoten_2 = kante.getKnoten_2().getId().split("\\.");
 
 			/*
-			 * Es muss jeweils 10 zu den Positionen aus den ArrayList hinzuaddiert werden, 
-			 * damit die Linien in den Knoten starten und enden. Sonst wuerden die Linien 
+			 * Es muss jeweils 10 zu den Positionen aus den ArrayList hinzuaddiert werden,
+			 * damit die Linien in den Knoten starten und enden. Sonst wuerden die Linien
 			 * knapp ausserhalb der Knoten starten und enden.
 			 * 
-			 * Die Zahlen aus den beiden Arrays "knoten_1" und "knoten_2" muessen um eins verkleinert werden, da die Zaehlung der IDs nicht bei null beginnt.
+			 * Die Zahlen aus den beiden Arrays "knoten_1" und "knoten_2" muessen um eins
+			 * verkleinert werden, da die Zaehlung der IDs nicht bei null beginnt.
 			 */
 			int x_position_1 = x_positionen.get(Integer.parseInt(knoten_1[0]) - 1)
 					.get(Integer.parseInt(knoten_1[1]) - 1) + 10;
@@ -182,5 +193,15 @@ public class Controller {
 			// zeichne die Kante
 			gc.strokeLine(x_position_1, y_position_1, x_position_2, y_position_2);
 		}
+		
+		// ueberschreibe den Kreis fuer den Start mit einem gruenen Kreis, in dessen Mitte ein grosses S steht
+		gc.setFill(Color.LIGHTGREEN);
+		gc.fillOval(x_positionen.get(0).get(0), y_positionen.get(0).get(0), kreisgroesse, kreisgroesse);
+		gc.strokeText("S", x_positionen.get(0).get(0) + 7, y_positionen.get(0).get(0) + 14);
+		
+		// ueberschreibe den Kreis fuer den Start mit einem roten Kreis, in dessen Mitte ein grosses Z steht
+		gc.setFill(Color.RED);
+		gc.fillOval(x_positionen.get(x_positionen.size() - 1).get(0), y_positionen.get(y_positionen.size() - 1).get(0), kreisgroesse, kreisgroesse);
+		gc.strokeText("S", x_positionen.get(x_positionen.size() - 1).get(0) + 7, y_positionen.get(y_positionen.size() - 1).get(0) + 14);
 	}
 }
